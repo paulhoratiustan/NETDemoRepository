cf6f32e142af789c33cf5b82a10dcf87e1b6e8e7
paulhoratiustan <paul.stan@bridgevine.com>
2016-08-16 14:42:08 +0300

    Add new function

--- DemoProject/DemoProject/Program.cs
+++ DemoProject/DemoProject/Program.cs
@@ -8,10 +8,22 @@ namespace DemoProject
 {
     class Program
     {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
         static void Main(string[] args)
         {
-
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("x=");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("y=");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("x+y=" + Add(x, y));
         }
     }
 }
