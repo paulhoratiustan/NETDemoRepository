854e194e60fe89631d380f29882b633be8ef9941
paulhoratiustan <paul.stan@bridgevine.com>
2016-09-16 16:32:22 +0300

    Commit changes

--- DemoProject/DemoProject/Program.cs
+++ DemoProject/DemoProject/Program.cs
@@ -1,38 +1,48 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DemoProject
 {
     class Program
     {
+        private static Operation _add;
+
+        delegate int Operation(int x, int y);
+
+        //add 2 numbers
         public static int Add(int x, int y)
         {
 
             return x + y;
         }
 
+
+        //substract 2 numbers
         public static int Sub(int x, int y)
         {
 
             return x - y;
         }
 
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            ParallelQuery<int> list = new List<int>(new int[100]).AsParallel();
 
-            Console.WriteLine("x=");
-            int x = Convert.ToInt32(Console.ReadLine());
+            list.ForAll(x =>
+            {
+                //Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                //Thread.Sleep(1 * 1000);
+            });
 
-            Console.WriteLine("y=");
-            int y = Convert.ToInt32(Console.ReadLine());
+            _add = (x,y) => {
+                x = x - 2;
+                return x + y;
+            };
 
-            Console.WriteLine("x + y = " + Add(x, y));
+            Console.WriteLine(_add(1, 200));
+            
 
-            Console.WriteLine("x - y = " + Sub(x, y));
         }
     }
 }
