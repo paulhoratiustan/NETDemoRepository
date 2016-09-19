750f0015ad37483ea7681e1b1f0b82d0d637e445
paulhoratiustan <paul.stan@bridgevine.com>
2016-09-19 13:34:07 +0300

    Anoither commit on Branch 2

--- DemoProject/DemoProject/Person.cs
+++ DemoProject/DemoProject/Person.cs
@@ -5,6 +5,7 @@
         public override string ToString()
         {
             //Should be committed on Branch 2
+            //Another commit on Branch 2
             return "this is a Person.";
         }
     }
