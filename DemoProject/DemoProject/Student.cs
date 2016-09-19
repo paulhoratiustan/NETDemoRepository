9b71e47d1ce3d14e7d237213d9bcb676441a84f5
paulhoratiustan <paul.stan@bridgevine.com>
2016-09-19 13:23:32 +0300

    Commit on master

---
+++ DemoProject/DemoProject/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+    class Student : Person
+    {
+        private string _name = null;
+        private string Name
+        {
+            get { return value; }
+            set { value = }
+        }
+    }
+}
